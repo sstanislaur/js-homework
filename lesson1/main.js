3021e2225d48e4fe4d31f354fb198491f3a17132
sstanislaur <lisnychukstas@gmail.com>
2023-02-01 13:40:51 -0800

    Initial commit

---
+++ lesson1/main.js
@@ -0,0 +1,35 @@
+let a = 'hello';
+let b = 'owu.com.ua';
+console.log(a, b);
+
+let c = 1;
+let c1 = 10;
+let c2 = -999;
+let c3 = 123;
+let c4 = 3.14;
+let c5 = 2.7;
+let c6 = 16;
+console.log(c, c1, c2, c3, c4, c5, c6);
+
+let d = true;
+console.log(d);
+
+let s = false;
+console.log(s);
+
+
+let firstname = 'Lisnychuk';
+let middlename = 'Stas';
+let lastname = 'Mykolayovych';
+let person = ` ${firstname} ${middlename} ${lastname}`;
+console.log(person);
+
+
+// typeof
+let q = 100;
+console.log(typeof q);
+let w = '100';
+console.log(typeof w);
+let e = true;
+console.log(typeof e);
+
---
+++ lesson2/main.js
@@ -0,0 +1,24 @@
+let a
+a = [1, 12, 123, 1234, 12345, 123456, 1234567, 12345678, 123456789, 1234567890];
+console.log(a)
+
+let person = {
+    title: 'Garry Poter',
+    pageCount: '50',
+    genre: 'fantasy',
+}
+console.log(person);
+
+let person1 = {
+    title: 'Garry Poter',
+    pageCount: '50',
+    genre: 'fantasy',
+    authors: ['name','age']
+}
+console.log(person1);
+
+let users = [
+    {user: name='vasya'}
+]
+
+console.log(users);
