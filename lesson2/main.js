e833817bdd5689ffaad520dcd5e442e4433a0666
sstanislaur <lisnychukstas@gmail.com>
2023-02-07 10:36:42 -0800

    1

--- lesson2/main.js
+++ lesson2/main.js
@@ -1,7 +1,16 @@
 // - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
 let a
 a = [1, 'asd', 123, 'qwe', 12345, 'tyu', 1234567, 'zxc', 123456789, 'qwea'];
-console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7]);
+console.log(a[8]);
+console.log(a[9]);
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
 let person = {
     title: 'Garry Poter',
@@ -9,6 +18,18 @@ let person = {
     genre: 'fantasy'
 }
 console.log(person);
+let person2 = {
+    title: 'Garry Poter',
+    pageCount: 3407,
+    genre: 'fantasy'
+}
+console.log(person2);
+let person3 = {
+    title: 'Garry Poter',
+    pageCount: 3407,
+    genre: 'fantasy'
+}
+console.log(person3);
 
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - являється  масивом.
 // Кожен автор має поля name та age.
@@ -59,16 +80,16 @@ x !== 0 ? console.log(true) : console.log(false);
 // - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
 // (в першу, другу, третю или четверту частину години).
 let time = prompt('enter number');
-if (time >= 0 && time <= 14) {
+if (time > 0 && time <= 14) {
     console.log('В першу частину години.');
 }
-if (time >= 15 && time <= 30) {
+if (time > 15 && time <= 30) {
     console.log('В другу частину години.');
 }
-if (time >= 31 && time <= 45) {
+if (time > 31 && time <= 45) {
     console.log('В третю частину години.');
 }
-if (time >= 46 && time <= 59) {
+if (time > 46 && time <= 59) {
     console.log('В четверту частину години.');
 }
 
@@ -76,13 +97,13 @@ if (time >= 46 && time <= 59) {
 // - У змінній day дано якесь число від 1 до 31.
 // Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
 let day = prompt('enter number');
-if (day >= 1 && day <= 11) {
+if (day > 1 && day <= 11) {
     console.log('У першу декаду');
 }
-if (day >= 12 && day <= 22) {
+if (day > 12 && day <= 22) {
     console.log('У другу декаду');
 }
-if (day >= 23 && day <= 31) {
+if (day > 23 && day <= 31) {
     console.log('У третю декаду');
 }
 
@@ -133,26 +154,38 @@ if (z === c) {
 
 // - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
 // за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
-let b;
-if (b === 0 || b === false || b === '' || b === `` || b === "" || b === undefined || b === -0 || b === NaN || b === null) {
-    b = b || 'default';
-}
-console.log(b);
+let b = ''
+let b1 = b || 'defoult';
+console.log(b1);
 
 // - з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray.
 // За допомоги іф перевірити кожен його елемент на тривалість навчання.
 // У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
 
-let coursesAndDurationArray = {
-    title__JavaScriptComplex__MonthDuration: 5,
-    title__JavaComplex__MonthDuration: 6,
-    title__PythonComplex__MonthDuration: 6,
-    title__QAComplex__MonthDuration: 4,
-    title__FullStack__MonthDuration: 7,
-    title__Frontend__MonthDuration: 4,
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+if (coursesAndDurationArray[0].monthDuration > 5) {
+    console.log('Супер');
 }
-for (let key in coursesAndDurationArray) {
-    if (coursesAndDurationArray[key] !== 4 && coursesAndDurationArray[key] !== 5) {
-        console.log(key + ' Супер ')
-    }
+if (coursesAndDurationArray[1].monthDuration > 5) {
+    console.log('Супер');
+}
+if (coursesAndDurationArray[2].monthDuration > 5) {
+    console.log('Супер');
+}
+if (coursesAndDurationArray[3].monthDuration > 5) {
+    console.log('Супер');
+}
+if (coursesAndDurationArray[4].monthDuration > 5) {
+    console.log('Супер');
+}
+if (coursesAndDurationArray[5].monthDuration > 5) {
+    console.log('Супер');
 }
